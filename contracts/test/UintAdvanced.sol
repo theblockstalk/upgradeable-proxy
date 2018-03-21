3f38ad05397a799c1b93053f1af5762097142f32
Jack Tanner <jack@jackandtheblockstalk.com>
2018-03-21 13:03:46 +0000

    changed UintAdvanced functions to not inherit

--- contracts/test/UintAdvanced.sol
+++ contracts/test/UintAdvanced.sol
@@ -14,23 +14,42 @@ contract UintAdvancedV1 is Upgradeable {
     }
 }
 
-contract UintAdvancedV2a_NewFunction is UintAdvancedV1 {
+contract UintAdvancedV2a_NewFunction is Upgradeable {
+    uint value;
+
+    function getValue() view public returns (uint) {
+        return value;
+    }
+
+    function setValue(uint _value) public {
+        value = _value;
+    }
 
     function setDoubleValue(uint _value) public {
         value = 2*_value;
     }
 }
 
-contract UintAdvancedV2b_NewStorage is UintAdvancedV1 {
+contract UintAdvancedV2b_NewStorage is Upgradeable {
+    uint value;
     uint value2 = 100;
+
+    function getValue() view public returns (uint) {
+        return value;
+    }
 
     function setValue(uint _value) public {
         value = _value + value2;
     }
 }
 
-contract UintAdvancedV2c_NewEvent is UintAdvancedV1 {
+contract UintAdvancedV2c_NewEvent is Upgradeable {
+    uint value;
     event EventSetValue(uint newValue, uint oldValue);
+
+    function getValue() view public returns (uint) {
+        return value;
+    }
 
     function setValue(uint _value) public {
         EventSetValue(_value, value);
@@ -38,7 +57,9 @@ contract UintAdvancedV2c_NewEvent is UintAdvancedV1 {
     }
 }
 
-contract UintAdvancedV2d_ReverseFunctionOrder is UintAdvancedV1 {
+contract UintAdvancedV2d_ReverseFunctionOrder is Upgradeable {
+    uint value;
+
     function setValue(uint _value) public {
         value = _value;
     }
@@ -46,18 +67,30 @@ contract UintAdvancedV2d_ReverseFunctionOrder is UintAdvancedV1 {
     function getValue() view public returns (uint) {
         return value;
     }
-
-
 }
 
-contract UintAdvancedV2e_NewStorage is UintAdvancedV1 {
+contract UintAdvancedV2e_NewStorage is Upgradeable {
+    uint value;
     uint value2 = 100;
 
     function getValue() view public returns (uint) {
         return value + value2;
     }
+
+    function setValue(uint _value) public {
+        value = _value;
+    }
 }
 
-contract UintAdvancedV2f_NewStorage is UintAdvancedV1 {
+contract UintAdvancedV2f_NewStorage is Upgradeable {
+    uint value;
     uint value2 = 100;
+
+    function getValue() view public returns (uint) {
+        return value;
+    }
+
+    function setValue(uint _value) public {
+        value = _value;
+    }
 }
